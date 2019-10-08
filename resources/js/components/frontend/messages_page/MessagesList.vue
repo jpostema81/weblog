2e3ccf3436d3fd355ab8258ebb6292dfa728a894
Jeroen Postema <jeroen@script.nl>
2019-10-08 17:33:56 +0200

    minor changes (user & message resources added)

--- resources/js/components/frontend/messages_page/MessagesList.vue
+++ resources/js/components/frontend/messages_page/MessagesList.vue
@@ -1,24 +1,26 @@
 <template>
     <div>
         <div class="card article" v-for="(message, messageKey) in messages" v-bind:key="message.id">
-            <div class="card-content">
+            <div class="card-body">
                 <div class="media">
-                    <div class="media-content has-text-centered">
-                        <p class="title is-4">
+                    <div class="media-body text-center">
+                        <p class="card-title title is-4">
                             <a v-bind:href="'/messages/' + message.id">{{ message.title }}</a>
                         </p>
-                        <div class="tags has-addons level-item">
-                            <span class="tag is-rounded is-info">@ {{ message.user.name }}</span>
+                        
+                        <div>
+                            <span class="badge badge-pill badge-default">@ {{ message.user.first_name }}</span>
 
-                                <span v-for="(category, categoryKey) in message.categories" v-bind:key="category.id" class="tag is-black">
-                                    {{ category.name }}<span v-if="categoryKey != message.categories.length - 1">,</span>
-                                </span>
+                            <span v-for="(category, categoryKey) in message.categories" v-bind:key="category.id" class="tag is-black">
+                                {{ category.name }}<span v-if="categoryKey != message.categories.length - 1">,</span>
+                            </span>
 
                             <span class="tag is-rounded">{{ moment(message.created_at) }}</span>
                         </div>
+                        
                     </div>
                 </div>
-                <div class="content article-body">
+                <div class="card-text">
                     {{ message.content.slice(0, 240) }}...
                     <div v-if="message.image">
                         <img v-bind:src="'/storage/' + message.image">
@@ -27,11 +29,6 @@
             </div>
         </div>
     </div>
-            
-    <!-- <span v-for="(category, key) in categories" v-bind:key="category.id">
-        <input class="is-checkradio" :id="'category_'+key" type="checkbox" name="categoryFilter[]" checked="checked" :value="category.id" @change="filterMessages">
-        <label :for="'category_'+key">{{ category.name }}</label>
-    </span> -->
 </template>
 
 <script>
