c8da19d52c48cb53ab4c031b29a14ee329c66baa
Jeroen Postema <jeroen@script.nl>
2019-10-15 09:40:11 +0200

    finish axios interceptors code

--- resources/js/interceptors.js
+++ resources/js/interceptors.js
@@ -1,42 +1,51 @@
-Vue.http.interceptors.push((request, next) => {
+import axios from 'axios';
+
+axios.interceptors.request.use(config =>  
+{
     /**
      * Here we will fecth the token from local storage and 
      * attach it (if exists) to the Authorization header on EVERY request.
      */
     let token = window.localStorage.getItem('user-token');
 
-    if (token) {
-        request.headers = request.headers || {}
-        request.headers.Authorization = `Bearer ${token}`
+    if(token) 
+    {
+        config.headers['Authorization'] = 'Bearer ' + token;
     }
+
+    return config;
 
     /**
      * Here is where we can refresh the token.
      */
-    next((response) => {
-        /**
-         * If we get a 401 response from the API means that we are Unauthorized to
-         * access the requested end point. 
-         * This means that probably our token has expired and we need to get a new one.
-         */
-        if (response.status === 401) {
-            return Vue.http.get('http://api.dev/refresh-token').then((result) => {
-                // Save the new token on local storage
-                window.localStorage.setItem('user-token', result.data.token)
+    // next((response) => {
+    //     /**
+    //      * If we get a 401 response from the API means that we are Unauthorized to
+    //      * access the requested end point. 
+    //      * This means that probably our token has expired and we need to get a new one.
+    //      */
+    //     if (response.status === 401) {
+    //         return Vue.http.get('http://api.dev/refresh-token').then((result) => {
+    //             // Save the new token on local storage
+    //             window.localStorage.setItem('user-token', result.data.token)
 
-                // Resend the failed request whatever it was (GET, POST, PATCH) and return its resposne
-                return Vue.http(request).then((response) => {
-                    return response;
-                })
-            })
-            .catch(() => {
-                /**
-                 * We weren't able to refresh the token so the best thing to do is 
-                 * logout the user (removing any user information from storage)
-                 * and redirecting to login page
-                 */
-                return router.go({name: 'login'})
-            })
-        }
-    })
-})
+    //             // Resend the failed request whatever it was (GET, POST, PATCH) and return its resposne
+    //             return Vue.http(request).then((response) => {
+    //                 return response;
+    //             })
+    //         })
+    //         .catch(() => {
+    //             /**
+    //              * We weren't able to refresh the token so the best thing to do is 
+    //              * logout the user (removing any user information from storage)
+    //              * and redirecting to login page
+    //              */
+    //             return router.go({name: 'login'})
+    //         })
+    //     }
+    // })
+}, 
+function(error) 
+{
+    return Promise.reject(error);
+});
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -25,27 +25,5 @@
             MessagesList,
             Pager,
         },
-        created() {
-            // // if there is an old token from a previous login, restore it (set axios default headers with token)
-            // // so API calls are authenticated again
-            // const token = localStorage.getItem('user-token');
-
-            // if(token)
-            // {
-            //     // first validate if local token is still valid
-            //     this.$store.dispatch('AuthenticationStore/AUTHENTICATE_BY_TOKEN').then(data =>
-            //     {
-            //         if(data.status === '1') 
-            //         {
-            //             axios.defaults.headers.common['Authorization'] = token;
-            //         }
-            //         else 
-            //         {
-            //             // token is invalid, delete token from store
-            //             this.$store.dispatch('AuthenticationStore/AUTH_LOGOUT');
-            //         }
-            //     });
-            // }
-        }
     }
 </script>
